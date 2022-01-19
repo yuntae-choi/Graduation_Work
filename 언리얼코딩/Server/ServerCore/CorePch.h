8c07768287a28e415902ac31d2cbbdc25ef57381
yuntae-choi <jinyeong8229@gmail.com>
2022-01-19 11:39:30 +0900

    서버 작업 1차
    기초적인 기능들을 담당할 패킷 구현과 더미 클라이언트와 서버 연동을 위한 기초적인 틀 작성

---
+++ 언리얼코딩/Server/ServerCore/CorePch.h
@@ -0,0 +1,111 @@
+#pragma once
+
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();
+
+
+
+const short SERVER_PORT = 4000;
+
+const int  MAX_NAME_SIZE = 20;
+const int  MAX_CHAT_SIZE = 100;
+
+const char CS_PACKET_LOGIN = 1;
+const char CS_PACKET_MOVE = 2;
+const char CS_PACKET_ATTACK = 3;
+const char CS_PACKET_CHAT = 4;
+
+const char SC_PACKET_LOGIN_OK = 1;
+const char SC_PACKET_MOVE = 2;
+const char SC_PACKET_PUT_OBJECT = 3;
+const char SC_PACKET_REMOVE_OBJECT = 4;
+const char SC_PACKET_CHAT = 5;
+const char SC_PACKET_LOGIN_FAIL = 6;
+const char SC_PACKET_STATUS_CHANGE = 7;
+
+#pragma pack (push, 1)
+struct cs_packet_login {
+	unsigned char size;
+	char	type;
+	char	id[MAX_NAME_SIZE];
+	char	pw[MAX_NAME_SIZE];
+
+};
+
+struct cs_packet_move {
+	unsigned char size;
+	char	type;
+	char	direction;			// 0 : up,  1: down, 2:left, 3:right
+};
+
+struct cs_packet_attack {
+	unsigned char size;
+	char	type;
+};
+
+
+struct sc_packet_login_ok {
+	unsigned char size;
+	char type;
+	int		id;
+	short	x, y, z;
+	char	name[MAX_NAME_SIZE];
+
+};
+
+struct sc_packet_move {
+	unsigned char size;
+	char type;
+	int		id;
+	short  x, y;
+	int		move_time;
+};
+
+struct sc_packet_put_object {
+	unsigned char size;
+	char type;
+	int id;
+	short x, y;
+	char object_type;
+	char	name[MAX_NAME_SIZE];
+};
+
+struct sc_packet_remove_object {
+	unsigned char size;
+	char type;
+	int id;
+};
+
+struct sc_packet_chat {
+	unsigned char size;
+	char type;
+	int id;
+	char message[MAX_CHAT_SIZE];
+};
+
+struct sc_packet_login_fail {
+	unsigned char size;
+	char type;
+	int	 reason;		// 0: 중복 ID,  1:사용자 Full
+};
+
+struct sc_packet_status_change {
+	unsigned char size;
+	char type;
+	short	hp;
+};
+
+#pragma pack(pop)
