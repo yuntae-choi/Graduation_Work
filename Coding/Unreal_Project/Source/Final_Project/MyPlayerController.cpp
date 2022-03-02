240b83387d960c7fd4336d910f0a2d73e71838eb
wighs33 <genji@kakao.com>
2022-03-02 18:51:14 +0900

    게임로직 업로드

---
+++ Coding/Unreal_Project/Source/Final_Project/MyPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeGameOnly InputMode;
+	SetInputMode(InputMode);
+}
---
+++ Coding/Unreal_Project/Source/Final_Project/MyPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Final_Project.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINAL_PROJECT_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+};
