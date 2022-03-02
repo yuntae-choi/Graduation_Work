1ebd328f407ebc3ca8c5ffe6f0c40905b0843d78
wighs33 <genji@kakao.com>
2022-03-02 20:40:32 +0900

    connect 완료

--- Coding/Unreal_Project/Source/Final_Project/MyPlayerController.h
+++ Coding/Unreal_Project/Source/Final_Project/MyPlayerController.h
@@ -4,6 +4,7 @@
 
 #include "Final_Project.h"
 #include "GameFramework/PlayerController.h"
+#include "ClientSocket.h"
 #include "MyPlayerController.generated.h"
 
 /**
@@ -13,7 +14,12 @@ UCLASS()
 class FINAL_PROJECT_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AMyPlayerController();
 	
 protected:
 	virtual void BeginPlay() override;
+
+	ClientSocket*		Socket;			// 辑滚客 立加且 家南
+	bool				bIsConnected;	// 辑滚客 立加 蜡公
 };
