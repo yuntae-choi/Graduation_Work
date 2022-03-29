9fe418fe5dfb35da081656f8f9b98c21d7e777df
yuntae-choi <jinyeong8229@gmail.com>
2022-03-29 11:17:24 +0900

    코딩 폴더 삭제

--- Coding/Unreal_project2/Source/Final_Project/Final_Project.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Final_Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Final_Project, "Final_Project" );
-DEFINE_LOG_CATEGORY(Final_Project);
