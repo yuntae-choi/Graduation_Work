ad0b36af2e371651dc4330650d668a67a955f92c
rkdlqzz <rkdlqzz081@gmail.com>
2022-03-30 17:14:31 +0900

    눈덩이 던지는 시점에 이벤트 호출을 위한 인터페이스 추가

---
+++ Coding/Unreal_Project/Source/Final_Project/I_Throwable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "I_Throwable.h"
+
+// Add default functionality here for any II_Throwable functions that are not pure virtual.
