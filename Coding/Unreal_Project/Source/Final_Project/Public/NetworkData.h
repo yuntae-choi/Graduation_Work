276d3623debc11d859a5ca49d1e16303443ffe0c
yuntae-choi <jinyeong8229@gmail.com>
2022-04-13 18:23:21 +0900

    Revert "Update NetworkData.h"
    This reverts commit 9c7495918e41685ac787f1c82631c3f7045252cd.

--- Coding/Unreal_Project/Source/Final_Project/Public/NetworkData.h
+++ Coding/Unreal_Project/Source/Final_Project/Public/NetworkData.h
@@ -50,9 +50,7 @@ enum SC_PacketType
 	SC_PACKET_CHAT,
 	SC_PACKET_LOGIN_FAIL,
 	SC_PACKET_STATUS_CHANGE,
-	SC_PACKET_DISCONNECT,
-	SC_PACKET_HP
-
+	SC_PACKET_DISCONNECT
 };
 
 enum COMMAND_Type
@@ -158,14 +156,6 @@ struct sc_packet_status_change {
 	short	level;
 	short	hp, maxhp;
 	int		exp;
-};
-
-
-struct sc_packet_hp_change {
-	unsigned char size;
-	char type;
-	int target;
-	int	hp;
 };
 
 
