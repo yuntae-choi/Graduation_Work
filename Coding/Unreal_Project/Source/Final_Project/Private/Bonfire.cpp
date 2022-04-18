69b30b63bc81823ce9aca6e2895665dcf590a010
yuntae-choi <jinyeong8229@gmail.com>
2022-04-18 16:15:48 +0900

    모닥불 코드 복구

--- Coding/Unreal_Project/Source/Final_Project/Private/Bonfire.cpp
+++ Coding/Unreal_Project/Source/Final_Project/Private/Bonfire.cpp
@@ -3,6 +3,7 @@
 
 #include "Bonfire.h"
 #include "MyCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 const int ABonfire::iHealAmount = 10;
 const int ABonfire::iDamageAmount = 1;
@@ -31,6 +32,8 @@ ABonfire::ABonfire()
 void ABonfire::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UpdateOverlapCharacters(); // 처음 시작할 때는 begin overlap 이벤트가 발생하지 않아서 이렇게 처리해주어야 한다
 	
 }
 
@@ -45,7 +48,7 @@ void ABonfire::OnComponentBeginOverlap(class UPrimitiveComponent* OverlappedComp
 {
 	AMyCharacter* mycharacter = Cast<AMyCharacter>(OtherActor);
 	if (mycharacter)
-	{
+	{   // begin overlap 하는 캐릭터의 체력 증가하도록 변경
 		mycharacter->SetIsInsideOfBonfire(true);
 		mycharacter->UpdateTemperatureState();
 	}
@@ -55,8 +58,30 @@ void ABonfire::OnComponentEndOverlap(class UPrimitiveComponent* OverlappedComp, 
 {
 	AMyCharacter* mycharacter = Cast<AMyCharacter>(OtherActor);
 	if (mycharacter)
-	{
+	{   // end overlap 하는 캐릭터의 체력 감소하도록 변경
 		mycharacter->SetIsInsideOfBonfire(false);
 		mycharacter->UpdateTemperatureState();
+	}
+}
+
+void ABonfire::UpdateOverlapCharacters()
+{
+	TArray<AActor*> overlapActorsArray;	// overlap 중인 캐릭터들을 담을 배열
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes; // overlap 가능한 오브젝트 타입 - MyCharacter
+	TEnumAsByte<EObjectTypeQuery> MyCharacter = UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_GameTraceChannel1); // ECC_GameTraceChannel1 - MyCharacter
+	ObjectTypes.Add(MyCharacter);
+	TArray<AActor*> emptyArray;
+
+	// overlap 중인 캐릭터들 배열에 담기
+	UKismetSystemLibrary::ComponentOverlapActors(sphereComponent, GetActorTransform(), ObjectTypes, nullptr, emptyArray, overlapActorsArray);
+
+	for (AActor* actor : overlapActorsArray)
+	{	// overlap 중인 캐릭터들의 체력 증가하도록 변경
+		AMyCharacter* character = Cast<AMyCharacter>(actor);
+		if (character)
+		{
+			character->SetIsInsideOfBonfire(true);
+			character->UpdateTemperatureState();
+		}
 	}
 }
