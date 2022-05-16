0b1de6a088936fad95fba81df72542c8c151f067
rkdlqzz <rkdlqzz081@gmail.com>
2022-05-16 19:05:13 +0900

    토네이도 추가(begin overlap시 캐릭터 회전 및 z값 증가, end overlap시 랜덤한 방향으로 날아감)

---
+++ Coding/Unreal_Project/Source/Final_Project/Private/Tornado.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tornado.h"
+#include "MyCharacter.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ATornado::ATornado()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (!collisionComponent)
+	{
+		collisionComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleCollisionComponent"));
+		collisionComponent->SetCapsuleHalfHeight(250.0f);
+		collisionComponent->SetCapsuleRadius(100.0f);
+		collisionComponent->SetHiddenInGame(false);	// 디버깅용 (토네이도 범위 보이도록)
+
+		RootComponent = collisionComponent;
+	}
+
+	collisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ATornado::OnComponentBeginOverlap);
+	collisionComponent->OnComponentEndOverlap.AddDynamic(this, &ATornado::OnComponentEndOverlap);
+}
+
+// Called when the game starts or when spawned
+void ATornado::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATornado::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATornado::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ATornado::OnComponentBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AMyCharacter* mycharacter = Cast<AMyCharacter>(OtherActor);
+	if (mycharacter)
+	{
+		mycharacter->SetIsInTornado(true);
+		mycharacter->StartStun(3.5f);
+	}
+}
+
+void ATornado::OnComponentEndOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	AMyCharacter* mycharacter = Cast<AMyCharacter>(OtherActor);
+	if (mycharacter)
+	{
+		mycharacter->SetIsInTornado(false);
+
+		float randomFloat;
+		bool randomBool;
+
+		randomFloat = UKismetMathLibrary::RandomFloatInRange(600.0f, 1000.0f);
+		randomBool = UKismetMathLibrary::RandomBool();
+		float impulseX = UKismetMathLibrary::SelectFloat(randomFloat, -randomFloat, randomBool);
+
+		randomFloat = UKismetMathLibrary::RandomFloatInRange(600.0f, 1000.0f);
+		randomBool = UKismetMathLibrary::RandomBool();
+		float impulseY = UKismetMathLibrary::SelectFloat(randomFloat, -randomFloat, randomBool);
+
+		mycharacter->GetCharacterMovement()->AddImpulse(FVector(impulseX, impulseY, 1500.0f), true);
+	}
+}
---
+++ Coding/Unreal_Project/Source/Final_Project/Public/Tornado.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Tornado.generated.h"
+
+UCLASS()
+class FINAL_PROJECT_API ATornado : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATornado();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void OnComponentBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnComponentEndOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleDefaultsOnly, Category = Component)
+	UCapsuleComponent* collisionComponent;
+
+};
