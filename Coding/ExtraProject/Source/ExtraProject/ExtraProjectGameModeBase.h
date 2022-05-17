75794b0f9c2e20a7a409ee9f89e697205266e419
rkdlqzz <rkdlqzz081@gmail.com>
2022-05-17 15:31:07 +0900

    Extra Project 생성 (토네이도 위치용)

---
+++ Coding/ExtraProject/Source/ExtraProject/ExtraProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExtraProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXTRAPROJECT_API AExtraProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
